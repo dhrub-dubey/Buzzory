59f5e99b038d82a0bb3a19bd8c8bb230149880e2
Watson-Dhrub <dhrubdubey5997@gmail.com>
2026-04-28 21:46:48 +0530

    fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/buzzory/',
+  base: '/Buzzory/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
